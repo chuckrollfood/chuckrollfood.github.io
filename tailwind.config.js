7cad752b96d2ff2b617047e6bba8ebc771842a85
chuckrollfood-adm <chuckrollfood+githubadm@gmail.com>
2022-12-08 03:02:46 +0900

    add implementation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#ef5451",
+      secondary: "#ffdf00",
+      tertiary: "#ffb400",
+      chuckgray: "#6c6c6c"
+    }
+  },
+  plugins: [],
+};
